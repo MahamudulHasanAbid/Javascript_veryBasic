93cecaedf303a6cc94525df61d3ad69525c05818
Mahamudul Hasan <124416655+MahamudulHasanAbid@users.noreply.github.com>
2023-11-28 21:20:28 +0600

    Add files via upload

---
+++ operator.js
@@ -0,0 +1,24 @@
+console.log("Operators in JS")
+let a = 34;
+let b = 7;
+// Arithmatic operators
+console.log("a + b =", a+b)
+console.log("a - b =", a-b)
+console.log("a * b =", a*b)
+console.log("a / b =", a/b)
+console.log("a ** b =", a**b)
+console.log("a % b =", a%b)
+
+console.log("a++", a++)
+console.log("a", a)
+console.log("a--", a--)
+console.log("a", a)  
+
+// comparison operators
+let c1 = 5;
+let c2 = '5';
+
+console.log("comp1 == comp2", c1 == c2)
+console.log("comp1 != comp2", c1 == c2)
+console.log("comp1 === comp2", c1 === c2)
+console.log("comp1 !== comp2", c1 !== c2)
