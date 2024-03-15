264b0dd4330e9c35d9aece4cc1f6c5856b9c6416
Mahamudul Hasan <124416655+MahamudulHasanAbid@users.noreply.github.com>
2024-03-15 10:35:07 +0600

    Add files via upload

---
+++ DOM_harry_sigmaweb.js
@@ -0,0 +1,17 @@
+console.log("Hello world.")
+
+document.body.childNodes;
+
+let cont = document.body.childNodes[1];
+
+console.log(cont.childNodes);
+console.log(cont.childElementCount);
+console.log(cont.firstChild);
+console.log(cont.lastChild);
+console.log(cont.firstElementChild);
+console.log(cont.lastElementChild);
+console.log(cont.children);
+console.log(cont.children[4]);
+
+console.log(cont.children[4].previousElementSibling);
+
