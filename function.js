e32cedae4179d12e0323e30b1e1a73a2e0bda7df
Mahamudul Hasan <124416655+MahamudulHasanAbid@users.noreply.github.com>
2024-03-15 10:17:06 +0600

    Add files via upload

---
+++ function.js
@@ -0,0 +1,11 @@
+function name_variable(name) {
+    console.log("Hello  " + name + " Your account has been finished. Recharge soon.");
+    console.log("Hello  " + name + " Your recharge is Successfull.");
+    console.log("Hello  " + name + " Your account is locked.");
+    console.log("Hello  " + name + " You have successfully updated your account.");
+    
+}
+
+name_variable('Abid');
+console.log("----------------------------")
+name_variable('Abdullah ');
